3f135692e66fb091efa9572946e272b7fdf32358
Wilfried Bounsi <wilcoln99@gmail.com>
2018-04-16 19:56:29 +0100

    Initialization

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/deps.css">
+    <link rel="stylesheet" href="/css/app.css">
+    {{--  TODO: Change the title 'Example' --}}
+    <title>Example</title>
+</head>
+<body>
+    
+    <script src="/js/deps.js"></script>
+    <script src="/js/app.js"></script>
+</body>
+</html>
